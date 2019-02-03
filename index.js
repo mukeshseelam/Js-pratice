1a3a1e75dc852be4adf32292f10d9a836d8466c6
mukeshseelam <seelam.mukesh@gmail.com>
2019-02-03 15:01:55 +0530

    in tial upload

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Mukesh practice");
+
+
+let firstName ="Mukesh";
+let lastName;
+lastName="munna";
+
+let name={firstName:'srikanth',
+lastName:'munna'}
+
+function output(name,lastName){
+
+console.log('Hello mukesh'+ name+ ''+lastName);
+
+}
+
+output("munna","ram");
+
